596d522a80ca1044680ba4639e3899024a5d40e6
Abdur Rahman Mahmud <ar7543078@gmail.com>
2025-01-23 16:09:09 +0600

    Add profile page, authentication API, and layout components; integrate Tailwind CSS with DaisyUI

---
+++ src/components/Navber.jsx
@@ -0,0 +1,27 @@
+"use client"
+import Link from "next/link";
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+
+
+const Navber = () => {
+  return (
+    <div className="container mx-auto mb-12">
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <h3 className=" font-bold text-xl">Next Blog</h3>
+        </div>
+        <div className="flex-none">
+          <div className="flex items-center gap-4">
+            <Link className="border px-3 py-1.5 rounded-md" href={"/"}>Home</Link>
+            <Link className="border px-3 py-1.5 rounded-md" href={"/profile"}>Profile</Link>
+            <LoginLink className="border px-3 py-1.5 rounded-md">Login</LoginLink>
+            
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navber;
