c52304d13d649c28e6fd01f9566ee9b75309c1e4
Abdur Rahman Mahmud <ar7543078@gmail.com>
2025-01-24 15:44:06 +0600

    Update footer copyright text to reflect new branding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -47,8 +47,7 @@ const Footer = () => {
         </nav>
         <aside>
           <p>
-            Copyright © {new Date().getFullYear()} - All right reserved by ACME
-            Industries Ltd
+            Copyright © {new Date().getFullYear()} - All Right Reserved By <span className="underline font-semibold">Next Blog</span>
           </p>
         </aside>
       </footer>
