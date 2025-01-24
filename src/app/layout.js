c0e0063eebf38c10a168e473f58b10a51ab0f7dc
Abdur Rahman Mahmud <ar7543078@gmail.com>
2025-01-24 20:30:43 +0600

    Remove sticky positioning from Navber for improved layout consistency

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           <header>
-            <nav className="bg-gray-50 sm:px-8 sticky top-0 fixed">
+            <nav className="bg-gray-50 sm:px-8 ">
               <Navber />
             </nav> 
           </header>
